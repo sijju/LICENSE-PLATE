c07b5809082e2ffea4bfb3cfdeaba4e4b2cf9f4f
karthik <31014162+sijju@users.noreply.github.com>
2023-03-28 13:10:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./Screens/Home";
+import Login from "./Screens/Login";
 
+
+
+const Stack = createStackNavigator()
 export default function App() {
+  
+  SplashScreen.preventAutoHideAsync();
+  setTimeout(SplashScreen.hideAsync,1000)
+  
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+   <NavigationContainer>
+    
+     <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} options={{headerShown:false}} />
+      <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
+     </Stack.Navigator>
+   </NavigationContainer> 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      
+    
+    
